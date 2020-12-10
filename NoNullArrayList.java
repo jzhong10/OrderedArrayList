bb2fe37410d5ba8ec8f56a8419d2090532e1d447
jzhong10 <junzhong2003@gmail.com>
2020-12-10 18:21:05 -0500

    Implemented and tested add() method

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class NoNullArrayList<T> extends ArrayList<T> {
   private ArrayList<T> arr;
 
@@ -7,5 +9,12 @@ public class NoNullArrayList<T> extends ArrayList<T> {
 
   public NoNullArrayList(int capacity) {
     arr = new ArrayList<T>(capacity);
+  }
+
+  public boolean add(T item) {
+    if (item==null) {
+      throw new IllegalArgumentException("no Null values accepted");
+    }
+    return super.add(item);
   }
 }
